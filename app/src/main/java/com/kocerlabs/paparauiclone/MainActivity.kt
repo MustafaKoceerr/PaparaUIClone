b4d3ac5a16b186bdb66f0f66f728de1d87aba03d
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-10 13:48:22 +0300

    bottom navigation added

--- app/src/main/java/com/kocerlabs/paparauiclone/MainActivity.kt
+++ app/src/main/java/com/kocerlabs/paparauiclone/MainActivity.kt
@@ -5,16 +5,47 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.kocerlabs.paparauiclone.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
+
+        binding.bottomNavigationView.setupWithNavController(navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment,
+            )
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        /**
+         * Burada toolbar yardımıyla fragment'ların isimlerinin yukarıda görünmesini sağladık.
+         * Bu isimleri değiştirmek istersen, navGraph'a gidip oradan label'ları değiştirerek bunu yapabilirsin.
+         */
     }
 }
