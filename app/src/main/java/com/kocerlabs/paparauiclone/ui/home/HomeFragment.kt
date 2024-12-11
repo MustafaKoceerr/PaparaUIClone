1a5c23bd8615792f55097bba8481883bd49cd092
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-11 15:03:47 +0300

    home fragment added

---
+++ app/src/main/java/com/kocerlabs/paparauiclone/ui/home/HomeFragment.kt
@@ -0,0 +1,24 @@
+package com.kocerlabs.paparauiclone.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kocerlabs.paparauiclone.databinding.FragmentHomeBinding
+import com.kocerlabs.paparauiclone.ui.base.BaseFragment
+
+class HomeFragment: BaseFragment<FragmentHomeBinding>(){
+    override fun getFragmentBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+
+
+}
