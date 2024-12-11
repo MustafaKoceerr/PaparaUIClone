06b3ecf4b25a9815f9c8856f73768d991a0c5238
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-11 15:00:59 +0300

    password fragment logic editted

--- app/src/main/java/com/kocerlabs/paparauiclone/ui/auth/PasswordFragment.kt
+++ app/src/main/java/com/kocerlabs/paparauiclone/ui/auth/PasswordFragment.kt
@@ -6,12 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.View.OnKeyListener
 import android.view.ViewGroup
+import android.widget.EditText
+import com.kocerlabs.paparauiclone.R
 import com.kocerlabs.paparauiclone.databinding.FragmentPasswordBinding
 import com.kocerlabs.paparauiclone.ui.base.BaseFragment
 import com.kocerlabs.paparauiclone.util.goToBack
 
 class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
     val TAG = "PasswordFragment"
+    private lateinit var activityRootView: View
+    private var isSoftKeyboardOpen = false
+
     override fun getFragmentBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
@@ -20,23 +25,60 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        activityRootView = requireActivity().findViewById(R.id.main)
+
         binding!!.btnBack.setOnClickListener {
             goToBack()
         }
 
         provideEditTextFunctionality()
+//        observeSoftKeyboard()
+
+
+    }
+
+    // todo focus özelliklerini ayarla
+//    private fun View.showKeyboard(edttext: EditText) {
+//        val inputMethodManager =
+//            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        inputMethodManager.showSoftInput(edttext, InputMethodManager.SHOW_IMPLICIT)
+//    }
+//
+//    private fun observeSoftKeyboard() {
+//        ViewCompat.setOnApplyWindowInsetsListener(activityRootView) { _, insets ->
+//            isSoftKeyboardOpen = insets.isVisible(WindowInsetsCompat.Type.ime())
+//            // true ise klavye açık, false ise kapalı
+//            insets
+//        }
+//    }
+//
+//    private fun manageFocusable(edttext: EditText, openOrCLose: Boolean = true) {
+//        if (openOrCLose) openFocus(edttext) else closeFocus(edttext)
+//    }
+//
+//    private fun closeFocus(edttext: EditText) {
+//        with(binding!!) {
+//            edttext.focusable = View.NOT_FOCUSABLE
+//        }
+//    }
+//
+//    private fun openFocus(edttext: EditText) {
+//        with(binding!!) {
+//            edttext.focusable = View.FOCUSABLE
+//            edttext.isFocusableInTouchMode = true
+//        }
+//    }
+
+    private fun openFocusAndFocusEdtText(edttext: EditText) {
+        with(binding!!) {
+            edttext.focusable = View.FOCUSABLE
+            edttext.isFocusableInTouchMode = true
+            edttext.requestFocus()
+        }
     }
 
     private fun provideEditTextFunctionality() {
         with(binding!!) {
-
-            linearPassword.setOnClickListener {
-                // todo eğer soft keyboard kapalıysa kaldığı yerden devam et,
-                // todo: eğer soft keyboard açıksa, bir işlem yapma.
-                when {
-
-                }
-            }
 
             edtPass1.addTextChangedListener(object : CustomSAMTextWatcher {
                 override fun beforeTextChanged(
@@ -46,7 +88,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                     after: Int
                 ) {
                     if (after == 1) {
-                        edtPass2.requestFocus()
+                        openFocusAndFocusEdtText(edtPass2)
                         edtPass1.focusable = View.NOT_FOCUSABLE
                     }
                 }
@@ -61,7 +103,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                     after: Int
                 ) {
                     if (after == 1) {
-                        edtPass3.requestFocus()
+                        openFocusAndFocusEdtText(edtPass3)
                         edtPass2.focusable = View.NOT_FOCUSABLE
                     }
                 }
@@ -86,9 +128,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                             if (edtPass2.text.toString().isNullOrEmpty()) {
                                 // focusu 1'e ata ve 1'in içindeki text'i sil.
                                 edtPass1.setText("")
-                                edtPass1.focusable = View.FOCUSABLE
-                                edtPass1.isFocusableInTouchMode = true
-                                edtPass1.requestFocus()
+                                openFocusAndFocusEdtText(edtPass1)
                             }
                             return true
                         }
@@ -107,7 +147,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                     after: Int
                 ) {
                     if (after == 1) {
-                        edtPass4.requestFocus()
+                        openFocusAndFocusEdtText(edtPass4)
                         edtPass3.focusable = View.NOT_FOCUSABLE
                     }
                 }
@@ -120,9 +160,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                         if (event?.action == KeyEvent.ACTION_DOWN) {
                             if (edtPass3.text.toString().isNullOrEmpty()) {
                                 edtPass2.setText("")
-                                edtPass2.focusable = View.FOCUSABLE
-                                edtPass2.isFocusableInTouchMode = true
-                                edtPass2.requestFocus()
+                                openFocusAndFocusEdtText(edtPass2)
                             }
                             return true
                         }
@@ -141,7 +179,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                     after: Int
                 ) {
                     if (after == 1) {
-                        edtPass5.requestFocus()
+                        openFocusAndFocusEdtText(edtPass5)
                         edtPass4.focusable = View.NOT_FOCUSABLE
                     }
                 }
@@ -154,9 +192,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                         if (event?.action == KeyEvent.ACTION_DOWN) {
                             if (edtPass4.text.toString().isNullOrEmpty()) {
                                 edtPass3.setText("")
-                                edtPass3.focusable = View.FOCUSABLE
-                                edtPass3.isFocusableInTouchMode = true
-                                edtPass3.requestFocus()
+                                openFocusAndFocusEdtText(edtPass3)
                             }
                             return true
                         }
@@ -175,7 +211,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                     after: Int
                 ) {
                     if (after == 1) {
-                        edtPass6.requestFocus()
+                        openFocusAndFocusEdtText(edtPass6)
                         edtPass5.focusable = View.NOT_FOCUSABLE
                     }
                 }
@@ -188,9 +224,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                         if (event?.action == KeyEvent.ACTION_DOWN) {
                             if (edtPass5.text.toString().isNullOrEmpty()) {
                                 edtPass4.setText("")
-                                edtPass4.focusable = View.FOCUSABLE
-                                edtPass4.isFocusableInTouchMode = true
-                                edtPass4.requestFocus()
+                                openFocusAndFocusEdtText(edtPass4)
                             }
                             return true
                         }
@@ -220,9 +254,7 @@ class PasswordFragment : BaseFragment<FragmentPasswordBinding>() {
                         if (event?.action == KeyEvent.ACTION_DOWN) {
                             if (edtPass6.text.toString().isNullOrEmpty()) {
                                 edtPass5.setText("")
-                                edtPass5.focusable = View.FOCUSABLE
-                                edtPass5.isFocusableInTouchMode = true
-                                edtPass5.requestFocus()
+                                openFocusAndFocusEdtText(edtPass5)
                             }
                             return true
                         }
