ef9edc584670fb6a1d09baf72cb19864e7c9ffa0
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-11 15:01:06 +0300

    utils added

--- app/src/main/java/com/kocerlabs/paparauiclone/util/Util.kt
+++ app/src/main/java/com/kocerlabs/paparauiclone/util/Util.kt
@@ -1,8 +1,13 @@
 package com.kocerlabs.paparauiclone.util
 
+import android.app.Activity
+import android.content.Context
+import android.graphics.Rect
+import android.util.TypedValue
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavDirections
 import androidx.navigation.fragment.findNavController
+
 
 fun Fragment.goToOtherFragment(action: NavDirections) {
     findNavController().navigate(action)
@@ -11,3 +16,6 @@ fun Fragment.goToOtherFragment(action: NavDirections) {
 fun Fragment.goToBack() {
     findNavController().popBackStack()  // Gerideki fragment'a dön
 }
+
+
+
