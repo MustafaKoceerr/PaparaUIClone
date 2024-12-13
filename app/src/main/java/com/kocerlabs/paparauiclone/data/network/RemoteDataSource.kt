f7800b257f3483950cab54cce6266b824799649e
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-13 23:29:15 +0300

    Requirements for the API including header are defined

--- app/src/main/java/com/kocerlabs/paparauiclone/data/network/RemoteDataSource.kt
+++ app/src/main/java/com/kocerlabs/paparauiclone/data/network/RemoteDataSource.kt
@@ -1,16 +1,17 @@
 package com.kocerlabs.paparauiclone.data.network
 
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor() {
-companion object{
-     const val BASE_URL_DUMMY = "https://dummyjson.com/"
-     const val BASE_URL_GIT =
-        "https://raw.githubusercontent.com/MustafaKoceerr/PaparaUIClone/refs/heads/main/DummyData/"
+    companion object {
+        const val BASE_URL_DUMMY = "https://dummyjson.com/"
+        const val BASE_URL_GIT =
+            "https://raw.githubusercontent.com/MustafaKoceerr/PaparaUIClone/refs/heads/main/DummyData/"
 
-}
+    }
 
     fun <Api> buildApi(
         api: Class<Api>,
@@ -18,6 +19,19 @@ companion object{
     ): Api { // retrofit nesnesini yaratacak olan generic fonksiyon
         return Retrofit.Builder()
             .baseUrl(url)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(api)
+    }
+
+    fun <Api> buildApiWithHeader(
+        api: Class<Api>,
+        url: String,
+        client: OkHttpClient
+    ): Api { // retrofit nesnesini yaratacak olan generic fonksiyon
+        return Retrofit.Builder()
+            .baseUrl(url)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(api)
