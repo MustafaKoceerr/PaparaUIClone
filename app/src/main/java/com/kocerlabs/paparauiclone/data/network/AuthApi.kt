b5c790292b85ba89d659c031bff56889446802f5
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-13 23:25:20 +0300

    network auth operations defined

---
+++ app/src/main/java/com/kocerlabs/paparauiclone/data/network/AuthApi.kt
@@ -0,0 +1,15 @@
+package com.kocerlabs.paparauiclone.data.network
+
+import com.kocerlabs.simplifiedcodingmvvm.data.network.model.LoginRequest
+import com.kocerlabs.simplifiedcodingmvvm.data.network.model.LoginResponse
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface AuthApi {
+
+    @POST("user/login")
+    suspend fun login( // async olarak coroutine ile çağırmak için suspend yaptım.
+        @Body loginRequest: LoginRequest
+    ): LoginResponse
+
+}
