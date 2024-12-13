4e5ba884e5c81406eb6cb81d09142ea7a9129190
MustafaKoceerr <mustafakoceerr@hotmail.com>
2024-12-13 05:37:50 +0300

    stories data added

--- app/src/main/java/com/kocerlabs/paparauiclone/data/repository/HomeRepository.kt
+++ app/src/main/java/com/kocerlabs/paparauiclone/data/repository/HomeRepository.kt
@@ -1,6 +1,8 @@
 package com.kocerlabs.paparauiclone.data.repository
 
+import com.kocerlabs.paparauiclone.R
 import com.kocerlabs.paparauiclone.data.network.HomeApi
+import com.kocerlabs.paparauiclone.data.network.model.StoryModel
 import com.kocerlabs.paparauiclone.data.network.model.TransactionModel
 import javax.inject.Inject
 
@@ -10,4 +12,17 @@ class HomeRepository @Inject constructor(
 
     suspend fun getTransactions(): List<TransactionModel> = safeApiCall { api.getTransactions() }
 
+    fun getStories(): List<StoryModel> {
+        return listOf(
+            StoryModel(R.drawable.img_wrap_up_papara_cashback, "Cashback"),
+            StoryModel(R.drawable.onboarding_illustration_4, "Duyurular"),
+            StoryModel(R.drawable.img_wrap_up_invite_friend, "Davet Et,\nKazan"),
+            StoryModel(R.drawable.img_wrap_up_international_transfer, "Yurt Dışı Para\nTransferi"),
+            StoryModel(R.drawable.img_wrap_up_papara_chat, "Sohbetler"),
+            StoryModel(R.drawable.img_wrap_up_papara_international, "Seyahat\nAyrıcalıkları"),
+            StoryModel(R.drawable.img_precious_metal_cashback_settings, "Kıymetli\nMadenler"),
+            StoryModel(R.drawable.img_wrap_up_cards, "Kartını Tasarla"),
+            StoryModel(R.drawable.img_wrap_up_papara_iban, "Aylık Özet"),
+        )
+    }
 }
